2f55951754af98ceb3e183323cc185b1160255e6
yaoshunqing <yaoshunqing0106@gmail.com>
2020-05-25 19:43:17 +0800

    new

--- first.py
+++ first.py
@@ -1,11 +1,6 @@
-# @Time : 2020/5/3 17:30 
-# @Author : Yao
-# @File : first.py
-# @Software: PyCharm
-import time
-
-while 1:
-    print("Hello") #你好
-    time.sleep(10)
-
-
+#Hello world
+#
+#
+###
+####
+#####
